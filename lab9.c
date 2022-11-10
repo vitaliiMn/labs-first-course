0e11314a4a5784b5686f117b6c115b29978cf465
vitaliiMn <115878020+vitaliiMn@users.noreply.github.com>
2022-11-10 19:28:54 +0300

    Update lab9.c

--- lab9.c
+++ lab9.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 
-void MinMax (int *min, int *max, int *v1, int *v2, int *v3){
+int Max (int v1, int v2, int v3){
+    
+    int max = v1;
+    
+    if (v2 > max)
+    max = v2;
+    
+    if (v3 > max)
+    max = v3;
+    
+    return max;
+}
 
-    *max = *v1;
-    *min = *v1;
- 
-    if (*v2 > *max)
-        *max = *v2;
-    if (*v3 > *max)
-        *max = *v3;
-    if (*v2 < *min)
-        *min = *v2;
-    if (*v3 < *min)
-        *min = *v3;
-        
+int Min (int v1, int v2, int v3){
+    int min = v1;
+    
+    if (v2 < min)
+    min = v2;
+    
+    if (v3 < min)
+    min = v3;
+    
+    return min;
 }
 
 void Move (int k, int *iUk, int *jUk, int *lUk){
@@ -23,17 +32,13 @@ void Move (int k, int *iUk, int *jUk, int *lUk){
     int i  = *iUk, j = *jUk, l = *lUk;
 
     iNew = (i * i * i - j * j * j + l * l * l - k) % 20;
-    v1= i * j * l -k;
-    v2= i * i * l -k;
-    v3= j * l * l -k;
-
-    int min, max;
-
-    MinMax(& min, & max, & v1, & v2, & v3);
-
-    jNew = min%30;
-    lNew = max%30;
+    v1= i * j * l - k;
+    v2= i * i * l - k;
+    v3= j * l * l - k;
     
+    jNew = Min (v1, v2, v3) % 30;
+    lNew = Max (v1, v2, v3) % 30;
+     
     *iUk = iNew;
     *jUk = jNew;
     *lUk = lNew;
