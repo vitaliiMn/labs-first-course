4dbf370dd38e74ab6583ccecb790637d5d45926e
vitaliiMn <115878020+vitaliiMn@users.noreply.github.com>
2022-11-13 19:33:13 +0300

    Update lab12.c

--- lab12.c
+++ lab12.c
@@ -1,24 +1,20 @@
 //18 вариант
 #include <stdio.h>
 
-long long Pow(int k){
+long long Pow(int k) {
     long long s = 1;
     int i;
-
     for (i = 0; i < k; ++i) 
         s *= 10;
-        
     return s;
 }
 
-int Size(long long s){
+int Size(long long s) {
     int k = 0;
-
-    while(s != 0){
+    while (s != 0) {
         s = s / 10;
         ++k;
     }
-
     return k;
 }
 
@@ -27,26 +23,23 @@ int main() {
     int k;
 
     while (1) {
-        scanf ("%lld", &s);
+        scanf("%lld", &s);
         k = Size(s);
         if(k>= 3){
             b = s / Pow(k - 2);
             b = b % 10;
-
+            
             s -= b * Pow(k - 2);
-
+            
             a = s / 100;
             a = a % 10;
-
+            
             s += (b - a) * 100;
-
             s += a * Pow(k - 2);
 
             printf("%lld\n", s);
-
         } else
             printf("%lld\n", s);    
     }
-    
     return 0;
 }
