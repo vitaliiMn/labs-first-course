e029d35d2b0478489d7bb5f93eef5835c4313902
vitaliiMn <115878020+vitaliiMn@users.noreply.github.com>
2022-11-16 17:05:45 +0300

    Create lab14.c

---
+++ lab14.c
@@ -0,0 +1,28 @@
+//19 вариант
+#include <stdio.h>
+
+#define maxn 100
+
+int main() {
+    int mass[maxn][maxn];
+    int i, l, n;
+
+    scanf("%i",&n);
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            scanf("%d", &mass[i][j]);     
+    for (l = 0; l < n; ++l){
+        for (i = 0; i <= l; ++i)
+            if (l % 2 == 0)
+                printf("%d ", mass[l][i]);  
+            else 
+                printf("%d ", mass[i][l]);     
+        for (i = 0; i < l; ++i)
+            if (l % 2 == 0)
+                printf("%d ", mass[l - i - 1][l]);
+            else 
+                printf("%d ", mass[l][l - i - 1]);   
+    }
+    putchar('\n');
+    return 0;
+}
