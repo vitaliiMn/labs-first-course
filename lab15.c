8922db16908016f5f78d50c37aba792e3d81a608
vitaliiMn <115878020+vitaliiMn@users.noreply.github.com>
2022-11-17 23:04:24 +0300

    Update lab15.c

--- lab15.c
+++ lab15.c
@@ -5,25 +5,34 @@
 
 int main() {
     int mass[MAX_N][MAX_N];
-    int i, j, n, min, sum;
+    short int columnCheck;
+    int i, j, l, n, min, sum;
 
     scanf("%i", &n);
     for (i = 0; i < n; ++i)
         for (j = 0; j < n; ++j)
-            scanf("%d", &mass[i][j]);  
+            scanf("%d", &mass[i][j]); 
 
-    for (i = 0; i < n; ++i) {
+    min = mass[0][0];
+    for (i = 0; i < n; ++i)    
+        for (j = 0; j < n; ++j) 
+            if (min > mass[i][j])
+                min = mass[i][j];
+
+    for (i = 0; i < n; ++i) { 
+        columnCheck = 0;
         sum = 0;
-        min = mass[0][i];
-        for (j = 0; j < n; ++j) {
-            sum += mass[j][i];
-            if (min > mass[j][i])
-                min = mass[j][i];
-        } 
-        for (j = 0; j < n; ++j)
-            if (mass[j][i] == min)
-                mass[j][i] = sum;
-    }
+        for (j = 0; j < n; ++j) 
+            if (min == mass[j][i]) {
+                if (!columnCheck){
+                    for (l = 0; l < n; ++l)
+                        sum += mass[l][i];
+                    mass[j][i] = sum;    
+                    columnCheck = 1;
+                } else 
+                    mass[j][i] = sum;     
+            }   
+    } 
     for (i = 0; i < n; ++i) {
         for (j = 0; j < n; ++j)
             printf("%d ", mass[i][j]);  
