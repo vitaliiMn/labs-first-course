431bf56982861c3a45ee8214c9ba8a710b398f3c
vitaliiMn <115878020+vitaliiMn@users.noreply.github.com>
2022-11-17 12:26:13 +0300

    Update lab14.c

--- lab14.c
+++ lab14.c
@@ -7,7 +7,7 @@ int main() {
     int mass[maxn][maxn];
     int i, l, n;
 
-    scanf("%i",&n);
+    scanf("%i", &n);
     for (int i = 0; i < n; ++i)
         for (int j = 0; j < n; ++j)
             scanf("%d", &mass[i][j]);     
